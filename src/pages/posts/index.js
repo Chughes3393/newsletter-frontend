99b7d3569416e7724e8e269e052bfc291badaee8
chughes3393 <hughes.c33@gmail.com>
2022-03-01 15:59:45 -0500

    added local-host Server, more components

--- src/pages/posts/index.js
+++ src/pages/posts/index.js
@@ -1,10 +1,76 @@
 import React from 'react';
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import Postf from '../../components/post-form/index'
 
 const Posts = () => {
+
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        fetchPosts()
+    }, [])
+
+    const fetchPosts = async () => {
+        try {
+            const response = await axios.get('http://localhost:8081/api/v1/newsletter/allposts')
+
+            // console.log(response)
+            setPosts(response.data)
+
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    const deletePosts = async (id) => {
+        try {
+            const response = await axios.delete(`http://localhost:8081/api/v1/newsletter/post/${id}`)
+            fetchPosts()
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
     return (
+        <>
+        <Postf />
+        
         <div>
-            This is our Posts Page
+            <table className="table table-dark table-striped">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Title</th>
+      <th scope="col">Subtitle</th>
+      <th scope="col">Content</th>
+      <th scope="col">Edit</th>
+    </tr>
+  </thead>
+  <tbody>
+      {
+          posts.map(posts => {
+                return (
+                    <tr key={posts.id}>
+                    <th scope="row">1</th>
+                    <td data-label="Title">{posts.title}</td>
+                    <td data-label="Subtitle">{posts.subtitle}</td>
+                    <td data-label="Content">{posts.content}</td>
+                    <td data-label="Edit">
+                    <i className="bi bi-trash" onClick={() => deletePosts(posts.id)} ></i>
+
+                    <i className="bi bi-pen" 
+                    ></i>
+                    </td>
+                  </tr>
+                )
+          })
+      }
+
+  </tbody>
+</table>
         </div>
+        </>
     );
 }
 
--- src/pages/subscribers/index.js
+++ src/pages/subscribers/index.js
@@ -1,10 +1,85 @@
 import React from 'react';
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import SubscriberF from '../../components/subscriber-form';
 
 const Subscribers = () => {
+
+    const [subscribers, setSubscribers] = useState([])
+
+    useEffect(() => {
+        fetchSubscribers()
+    }, [])
+
+    const fetchSubscribers = async () => {
+        try {
+            const response = await axios.get('http://localhost:8081/api/v1/newsletter/allsubscribers')
+
+            // console.log(response)
+            setSubscribers(response.data)
+
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    const deleteSubscribers = async (id) => {
+        try {
+            const response = await axios.delete(`http://localhost:8081/api/v1/newsletter/post/${id}`)
+            fetchSubscribers()
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    // const handleEdit = (subscribers) => {
+    //     setEditForm(true)
+    //     setSubscribersToEdit(subscribers)
+    // }
+
     return (
-        <div>
-            This is our Subscribers Page
-        </div>
+        <>
+            <SubscriberF
+                // fetchSubscribers={fetchSubscribers}
+                // editForm={editForm}
+                // subscriberToEdit={subscriberToEdit} 
+                />
+
+            <table className="table table-dark table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Edit</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {subscribers.map(subscribers => {
+                        return (
+                            <tr key={subscribers.id}>
+                                <th scope="row">1</th>
+                                <td data-label="First Name">{subscribers.firstname}</td>
+                                <td data-label="Last Name">{subscribers.lastname}</td>
+                                <td data-label="Email">{subscribers.email}</td>
+
+                                <td data-label="Edit">
+                                    <i className="bi bi-trash" onClick={() => deleteSubscribers(subscribers.id)} ></i>
+
+                                    <i className="bi bi-pen" 
+                                    // onClick={() => handleEdit(subscribers)}
+                                    ></i>
+                                </td>
+
+                            </tr>
+                        )
+                    })}
+
+                </tbody>
+            </table>
+        </>
     );
 }
 

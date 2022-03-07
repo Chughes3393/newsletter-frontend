1f2a6d942ca244874958ef28c9c3118e4448d93c
chughes3393 <hughes.c33@gmail.com>
2022-03-07 13:34:57 -0500

    navbar worx

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 import { useContext } from 'react'
-
 import UserContext from "../../contexts/UserContext";
 
 const Nav = () => {
@@ -9,44 +8,50 @@ const Nav = () => {
 
     return (
 
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <>
+            <div className='container'>
+                <nav className="navbar navbar-expand-sm navbar-light bg-light">
+                    <Link className="navbar-brand" to="/">Jumbalaya</Link>
 
-            <div className="container-fluid">
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
 
-                <Link className="navbar-brand" to="/">Jumbalaya</Link>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto">
 
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link className="nav-link" to="Contact">Contact</Link>
+                            </li>
 
-                        {
-                            !user ?
+                            {
+                                !user ?
 
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="login">Login</Link>
-                                </li>
+                                    <li className="nav-item">
+                                        <Link className="nav-link" to="login">Login</Link>
+                                    </li>
 
-                                :
+                                    :
 
-                                <li className="nav-item">
-                                    <Link className="nav-link" aria-current="page" to="posts">Posts</Link>
-                                </li>
-                        }
+                                    <li className="nav-item dropdown">
+                                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            Admin
+                                        </a>
 
-                        <li className="nav-item">
-                            <Link className="nav-link" to="subscribers">Subscribers</Link>
-                        </li>
+                                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                            <Link className="dropdown-item" to="subscribers">Subscribers</Link>
 
-                        <li className="nav-item">
-                            <Link className="nav-link" to="Contact">Contact</Link>
-                        </li>
+                                            <div className="dropdown-divider"></div>
+                                            <Link className="dropdown-item" to="posts">Posts</Link>
+                                        </div>
+                                    </li>
+                            }
+                        </ul>
+                    </div>
 
-                    </ul>
-                </div>
+                </nav>
             </div>
-        </nav>
+        </>
     );
 }
 
